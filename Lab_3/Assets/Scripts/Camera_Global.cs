845ac663e866240481cbee55ce5c9cba7776eb27
AddCondiments <addcondiments@gmail.com>
2017-09-29 13:52:15 -0400

    Lab_3
    Added bg, player controller,and 1st camera state.

---
+++ Lab_3/Assets/Scripts/Camera_Global.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Global : MonoBehaviour {
+
+	public int cameraMode;
+
+	public Transform player;
+
+	public Transform BG2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (cameraMode) {
+
+		default:
+		case 1:
+			this.transform.position = new Vector3 (player.position.x, 0, -10);
+			break;
+		case 2:
+			this.transform.position = new Vector3 (BG2.position.x, BG2.position.y, -10);
+			break;
+		case 3:
+			break;
+
+		}
+	}
+}
---
+++ Lab_3/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour {
+
+	public float speed = 2;
+
+	private Rigidbody2D arby;
+
+	// Use this for initialization
+	void Start () {
+		arby = this.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float hMove = Input.GetAxis ("Horizontal");
+		arby.velocity = new Vector2 (hMove, 0) * speed;
+	}
+}
